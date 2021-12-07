2dfe1038633405f3a9064c05f5e64ad06e23cad2
Kamil Świątek <86557701+kaman87410@users.noreply.github.com>
2021-12-08 00:22:55 +0100

    Game random number

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-var x = 2
-console.log(5 == 5);
+let number = Math.floor(Math.random() * 11);
+
+function play() {
+    let userNumber = document.getElementById("userNumber").value
+    let message;
+
+    if (userNumber > number) {
+        message = "Twój numer jest za duży";
+    } else if (userNumber < numer){
+        message = "Twój numer jest za mały";
+    } else{
+        message = "Gratulacje, liczba jest poprawna"
+    }
+
+    document.getElementById("message").innerHTML = message;
+}

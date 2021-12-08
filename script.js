8e2c8ed27bc1b9fe4cc1fe0435163eaa57f2f403
Kamil Świątek <86557701+kaman87410@users.noreply.github.com>
2021-12-08 09:44:46 +0100

    It works

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let number = Math.floor(Math.random() * 11);
+let number = Math.floor(Math.random()*10+1);
+
+
 
 function play() {
-    let userNumber = document.getElementById("userNumber").value
+    let userNumber = document.getElementById("userNumber").value;
     let message;
-
     if (userNumber > number) {
-        message = "Twój numer jest za duży";
-    } else if (userNumber < numer){
-        message = "Twój numer jest za mały";
-    } else{
+        message = "Twój numer jest za duży"
+    }
+    else if (userNumber < number) {
+        message = "Twój numer jest za mały"
+    } 
+    if (userNumber == number) {
         message = "Gratulacje, liczba jest poprawna"
     }
 
     document.getElementById("message").innerHTML = message;
-}
+    console.log(number)
+};
